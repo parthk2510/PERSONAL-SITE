2882d39940a853f11fa8d57754165a04e5e5a67d
parthk2510 <87540263+parthk2510@users.noreply.github.com>
2021-09-27 18:42:03 +0530

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome  ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
